322cdbd1305efc1bcf1f03e8ea182dc0eaa101a8
Andrew Lee <ALee@Andrews-MacBook-Pro-6.local>
2014-01-18 02:17:16 -0500

    Added more styling

--- static/scripts/custom.js
+++ static/scripts/custom.js
@@ -53,10 +53,35 @@ $(window).on("resize",function(e){
     
 });
 
+// Transition the header to sticky class when you reach about section //
+
 $(function () {
     $("#about").waypoint(function () {
         $("#main-nav").toggleClass("sticky")
     }, {
         offset: 160
     })
+});
+
+// This function is supposed to shrink the size of the nav when you reach a certain point of the page //
+
+$(function(){
+    $('#main_nav').data('size','big');
+});
+
+$(window).scroll(function(){
+    var $nav = $('#main_nav');
+    if ($('body').scrollTop() > 0) {
+        if ($nav.data('size') == 'big') {
+            $nav.data('size','small').stop().animate({
+                height:'40px'
+            }, 600);
+        }
+    } else {
+        if ($nav.data('size') == 'small') {
+            $nav.data('size','big').stop().animate({
+                height:'100px'
+            }, 600);
+        }  
+    }
 });
